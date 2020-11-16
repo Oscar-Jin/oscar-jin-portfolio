7ea531373b3185185ccb2df2c69d72e48f593449
Oscar Jin <kintininn@gmail.com>
2020-11-16 23:05:14 +0900

    fix ecma version

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,11 @@ module.exports = {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
   },
-  parser: "babel-eslint"
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: "module",
   },
   plugins: ["react"],
