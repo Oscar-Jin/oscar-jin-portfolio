bf9641aa1a1e88da18b5bac787ebc86ac97226c0
Oscar Jin <kintininn@gmail.com>
2020-11-16 21:23:58 +0900

    draft 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,54 @@
+//  ──────────────────────────────────────────────────────────── import ───┐
 import React from "react"
+import tw, { styled } from "twin.macro"
+import SEO from "../components/advanced-optimizations/seo"
+import Footer from "../components/hero-layouts/Footer"
+import Header from "../components/page-structure/header"
+import Layout from "../components/page-structure/layout"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+// <───────────────────────────────────────────────────────────────────────┘
+
+//  ────────────────────────────────────────────────────────── tailwind ───┐
+const StretchToFill = styled.div`
+  ${tw`flex flex-col`}
+  min-height: calc(100vh - 114px)
+`
+const PageNotFound = styled.div`
+  ${tw`flex items-center flex-1 max-w-screen-xl py-20 mx-auto mb-16`}
+`
+const Section = styled.div`
+  ${tw`text-center`}
+`
+const Title = styled.h2`
+  ${tw`text-6xl font-semibold text-gray-500`}
+`
+const Subtitle = styled.h3`
+  ${tw`text-2xl font-bold capitalize`}
+`
+const Description = styled.p`
+  ${tw`mt-3 text-gray-500`}
+`
+
+// <───────────────────────────────────────────────────────────────────────┘
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Header />
+
+    <StretchToFill>
+      <PageNotFound>
+        <Section>
+          <Title>404</Title>
+          <Subtitle>page not found</Subtitle>
+          <Description>
+            The page you are looking for was moved, removed, renamed or might
+            never existed.
+          </Description>
+        </Section>
+      </PageNotFound>
+      <Footer />
+    </StretchToFill>
   </Layout>
 )
 
