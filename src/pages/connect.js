89c8e8f63da2e7ba2b5ae39e47385ba6e32b4003
Oscar Jin <kintininn@gmail.com>
2020-11-24 12:24:59 +0900

    add new page

---
+++ src/pages/connect.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/page-structures/layout"
+
+export default function Connect() {
+  return (
+    <Layout>
+      <div>connect</div>
+    </Layout>
+  )
+}
---
+++ src/pages/resume.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/page-structures/layout"
+
+export default function Resume() {
+  return (
+    <Layout>
+      <div>Resume</div>
+    </Layout>
+  )
+}
